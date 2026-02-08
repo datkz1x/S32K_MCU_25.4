87e204989aeb27c4ccd80570b4862c5918306127
Dat <kaledinwindskymod@gmail.com>
2026-02-08 23:22:29 +0700

    Initial commit

---
+++ blinkled/Debug_FLASH/Project_Settings/Startup_Code/startup.d
@@ -0,0 +1,14 @@
+Project_Settings/Startup_Code/startup.o: \
+ ../Project_Settings/Startup_Code/startup.c \
+ D:/S32K_MCU_Workspace/blinkled/include/startup.h \
+ D:/S32K_MCU_Workspace/blinkled/include/device_registers.h \
+ D:/S32K_MCU_Workspace/blinkled/include/s32_core_cm4.h \
+ D:/S32K_MCU_Workspace/blinkled/include/S32K144.h \
+ D:/S32K_MCU_Workspace/blinkled/include/S32K144_features.h \
+ D:/S32K_MCU_Workspace/blinkled/include/devassert.h
+D:/S32K_MCU_Workspace/blinkled/include/startup.h:
+D:/S32K_MCU_Workspace/blinkled/include/device_registers.h:
+D:/S32K_MCU_Workspace/blinkled/include/s32_core_cm4.h:
+D:/S32K_MCU_Workspace/blinkled/include/S32K144.h:
+D:/S32K_MCU_Workspace/blinkled/include/S32K144_features.h:
+D:/S32K_MCU_Workspace/blinkled/include/devassert.h:
---
+++ blinkled/Debug_FLASH/src/main.d
@@ -0,0 +1,2 @@
+src/main.o: ../src/main.c ../src/S32K144.h
+../src/S32K144.h:
